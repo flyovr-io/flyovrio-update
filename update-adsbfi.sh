15a6ccfd1bd88aeaf2007102a98aa1a8be2676d1
ADS-B related code <34561158+ogadsb@users.noreply.github.com>
2023-03-14 14:23:05 -0700

    Update update-adsbfi.sh

--- update-adsbfi.sh
+++ update-adsbfi.sh
@@ -33,7 +33,7 @@ function aptInstall() {
 packages="git make gcc libusb-1.0-0 libusb-1.0-0-dev librtlsdr0 librtlsdr-dev ncurses-bin ncurses-dev zlib1g zlib1g-dev python3-dev python3-venv libzstd-dev libzstd1"
 aptInstall $packages
 
-git clone --quiet --depth 1 https://github.com/tmantti/adsbfi-update.git
+git clone --quiet --depth 1 https://github.com/adsbfi/adsbfi-update.git
 cd adsbfi-update
 
 find skeleton -type d | cut -d / -f1 --complement | grep -v '^skeleton' | xargs -t -I '{}' -s 2048 mkdir -p /'{}' &>/dev/null
