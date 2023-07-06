a04bc9b77336177663000a07e2f2691b99ea0aa6
FLYOVR.IO <134643219+flyovr-io@users.noreply.github.com>
2023-07-06 18:23:19 -0500

    Flyovr.io Update

--- skeleton/usr/local/bin/flyovrio-feed.sh
+++ skeleton/usr/local/bin/flyovrio-feed.sh
@@ -2,14 +2,14 @@
 while wait
 do
     sleep 30 &
-    /usr/bin/adsbfifeeder --quiet --net --net-only \
+    /usr/bin/flyovriofeeder --quiet --net --net-only \
         --db-file=none --max-range 450 \
         --net-beast-reduce-interval 0.5 \
-        --net-connector feed.adsb.fi,30004,beast_reduce_plus_out \
+        --net-connector feed.flyovr.io,30004,beast_reduce_plus_out \
         --net-connector 127.0.0.1,30005,beast_in \
         --net-ro-interval 0.2 --net-ri-port 0 --net-ro-port 0 \
         --net-sbs-port 0 --net-bi-port 0 --net-bo-port 0 \
-        --json-location-accuracy 2 --write-json /run/adsbfi-feed \
+        --json-location-accuracy 2 --write-json /run/flyovrio-feed \
         --lat $LATITUDE --lon $LONGITUDE \
-        --uuid-file /boot/adsbfi-uuid
+        --uuid-file /boot/flyovrio-uuid
 done
