80208aad9714f432ade5fded372ea6db9117f671
FLYOVR.IO <134643219+flyovr-io@users.noreply.github.com>
2023-07-06 18:40:45 -0500

    Flyovr.io Update

--- update-flyovrio.sh
+++ update-flyovrio.sh
@@ -7,7 +7,7 @@ if [[ "$(id -u)" != "0" ]]; then
 fi
 
 # let's do all of this in a clean directory:
-updir=/tmp/update-adsbfi
+updir=/tmp/update-flyovrio
 
 rm -rf $updir
 mkdir -p $updir
@@ -33,30 +33,30 @@ function aptInstall() {
 packages="git make gcc libusb-1.0-0 libusb-1.0-0-dev librtlsdr0 librtlsdr-dev ncurses-bin ncurses-dev zlib1g zlib1g-dev python3-dev python3-venv libzstd-dev libzstd1"
 aptInstall $packages
 
-git clone --quiet --depth 1 https://github.com/adsbfi/adsbfi-update.git
-cd adsbfi-update
+git clone --quiet --depth 1 https://github.com/flyovr-io/flyovrio-update.git
+cd flyovrio-update
 
 find skeleton -type d | cut -d / -f1 --complement | grep -v '^skeleton' | xargs -t -I '{}' -s 2048 mkdir -p /'{}' &>/dev/null
 find skeleton -type f | cut -d / -f1 --complement | xargs -I '{}' -s 2048 cp -T --remove-destination -v skeleton/'{}' /'{}' >/dev/null
 
 # make sure the config has all the options, if not add them with default value:
-for line in $(grep -v -e '^#' -e '^$' boot-configs/adsbfi-config.txt); do
-    if ! grep -qs "$(echo $line | cut -d= -f1)" /boot/adsbfi-config.txt; then
-        echo $line >> /boot/adsbfi-config.txt
+for line in $(grep -v -e '^#' -e '^$' boot-configs/flyovrio-config.txt); do
+    if ! grep -qs "$(echo $line | cut -d= -f1)" /boot/flyovrio-config.txt; then
+        echo $line >> /boot/flyovrio-config.txt
     fi
 done
 
 # make sure things are executable before starting
-chmod a+x /usr/local/bin/adsbfi-create-uuid.sh 
-chmod a+x /usr/local/bin/update-adsbfi
-chmod a+x /usr/local/bin/adsbfi-feed.sh 
+chmod a+x /usr/local/bin/flyovrio-create-uuid.sh 
+chmod a+x /usr/local/bin/update-flyovrio
+chmod a+x /usr/local/bin/flyovrio-feed.sh 
 chmod a+x /usr/local/bin/pingfail.sh 
-chmod a+x /usr/bin/adsbfi-first-run
-chmod a+x /usr/bin/adsbfi-temp.sh
-chmod a+x /usr/bin/adsbfi-pi-exporter
-chmod a+x /usr/bin/adsbfi-978-start
-#chmod a+x /usr/bin/adsbfi-978
-#chmod a+x /usr/bin/adsbfifeeder
+chmod a+x /usr/bin/flyovrio-first-run
+chmod a+x /usr/bin/flyovrio-temp.sh
+chmod a+x /usr/bin/flyovrio-pi-exporter
+chmod a+x /usr/bin/flyovrio-978-start
+#chmod a+x /usr/bin/flyovrio-978
+#chmod a+x /usr/bin/flyovriofeeder
 
 
 # remove strange dhcpcd wait.conf in case it's there
@@ -66,11 +66,11 @@ systemctl daemon-reload
 
 # enable services
 systemctl enable \
-    adsbfi-first-run.service \
-    adsbfi-zt-enable.service \
+    flyovrio-first-run.service \
+    flyovrio-zt-enable.service \
     readsb.service \
-    adsbfi-mlat.service \
-    adsbfi-feed.service \
+    flyovrio-mlat.service \
+    flyovrio-feed.service \
     pingfail.service
 
 # mask services we don't need on this image
@@ -82,7 +82,7 @@ for service in $MASK; do
 done &>/dev/null
 
 cd $updir
-git clone --quiet --depth 1 https://github.com/adsbfi/readsb.git
+git clone --quiet --depth 1 https://github.com/flyovr-io/readsb.git
 
 echo 'compiling readsb (this can take a while) .......'
 
@@ -95,14 +95,14 @@ else
 fi
 
 echo 'copying new readsb binaries ......'
-cp -f readsb /usr/bin/adsbfifeeder
-cp -f readsb /usr/bin/adsbfi-978
+cp -f readsb /usr/bin/flyovriofeeder
+cp -f readsb /usr/bin/flyovrio-978
 cp -f readsb /usr/bin/readsb
 cp -f viewadsb /usr/bin/viewadsb
 
 
-echo 'make sure unprivileged users exist (readsb / adsbfi) ......'
-for USER in adsbfi readsb; do
+echo 'make sure unprivileged users exist (readsb / flyovrio) ......'
+for USER in flyovrio readsb; do
     if ! id -u "${USER}" &>/dev/null
     then
         adduser --system --home "/usr/local/share/$USER" --no-create-home --quiet "$USER"
@@ -119,14 +119,14 @@ chown readsb /var/globe_history
 
 echo 'restarting services .......'
 restartIfEnabled readsb
-restartIfEnabled adsbfi-feed
-restartIfEnabled adsbfi-978
+restartIfEnabled flyovrio-feed
+restartIfEnabled flyovrio-978
 
 cd $updir
 rm -rf $updir/readsb
 
-VENV=/usr/local/share/adsbfi/venv/
-if [[ -f /usr/local/share/adsbfi/venv/bin/python3.7 ]] && command -v python3.9 &>/dev/null;
+VENV=/usr/local/share/flyovrio/venv/
+if [[ -f /usr/local/share/flyovrio/venv/bin/python3.7 ]] && command -v python3.9 &>/dev/null;
 then
     rm -rf "$VENV"
 fi
@@ -136,7 +136,7 @@ mv "$VENV" "$VENV-backup" -f &>/dev/null || true
 cd $updir
 
 echo 'building mlat-client in virtual-environment .......'
-if git clone --quiet --depth 1 --single-branch https://github.com/adsbfi/mlat-client.git \
+if git clone --quiet --depth 1 --single-branch https://github.com/flyovr-io/mlat-client.git \
     && cd mlat-client \
     && /usr/bin/python3 -m venv $VENV  \
     && source $VENV/bin/activate  \
@@ -156,7 +156,7 @@ else
 fi
 
 echo 'starting services .......'
-restartIfEnabled adsbfi-mlat
+restartIfEnabled flyovrio-mlat
 
 cd $updir
 rm -f -R $updir/mlat-client
@@ -164,14 +164,14 @@ rm -f -R $updir/mlat-client
 cd $updir
 
 echo 'update tar1090 ...........'
-bash -c "$(wget -nv -O - https://raw.githubusercontent.com/adsbfi/tar1090/master/install.sh)"
+bash -c "$(wget -nv -O - https://raw.githubusercontent.com/flyovr-io/tar1090/master/install.sh)"
 
-if [[ -f /boot/adsbfi-config.txt ]]; then
-    if ! grep -qs -e 'GRAPHS1090' /boot/adsbfi-config.txt; then
-        echo "GRAPHS1090=yes" >> /boot/adsbfi-config.txt
+if [[ -f /boot/flyovrio-config.txt ]]; then
+    if ! grep -qs -e 'GRAPHS1090' /boot/flyovrio-config.txt; then
+        echo "GRAPHS1090=yes" >> /boot/flyovrio-config.txt
     fi
-    if ! grep -qs -e "ZEROTIER" /boot/adsbfi-config.txt; then
-        echo "ZEROTIER=no" >> /boot/adsbfi-config.txt
+    if ! grep -qs -e "ZEROTIER" /boot/flyovrio-config.txt; then
+        echo "ZEROTIER=no" >> /boot/flyovrio-config.txt
     fi
 fi
 
@@ -181,7 +181,7 @@ if ischroot; then
     exit 0
 fi
 
-echo "8.2.$(date '+%y%m%d')" > /boot/adsbfi-version-decoder
+echo "8.2.$(date '+%y%m%d')" > /boot/flyovrio-version-decoder
 
 echo '--------------------------------------------'
 echo '--------------------------------------------'
